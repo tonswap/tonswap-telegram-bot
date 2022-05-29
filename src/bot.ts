7537e0691dd527f2ac3d168077a76ac83d104c25
denis6421 <denis@orbs.com>
2022-05-29 16:40:43 +0300

    updates

--- src/bot.ts
+++ src/bot.ts
@@ -3,12 +3,14 @@ import {SessionStore} from "telegraf/src/session";
 import fs from "fs";
 import {Token} from "./tokens";
 import {Address} from "ton";
+import translations from './translations'
 
 const TonWeb = require('tonweb');
 const tokens = require('./tokens');
 const BN = require("bn.js");
 
-const TONSWAP_URL = 'https://tonswap.github.io/tonswap-web-new/';
+const TONSWAP_URL = 'https://tonswap.github.io/tonswap-web/';
+
 
 const tonweb = new TonWeb(
     new TonWeb.HttpProvider("https://scalable-api.tonwhales.com/jsonRPC")
@@ -19,13 +21,14 @@ interface SessionData {
     walletAddress: string,
     token?: string,
     language?: string,
+    walletBalance?: string; 
 }
 
 interface MyContext extends Context {
     session?: SessionData
 }
 
-const bot = new Telegraf<MyContext>(require('./config.ts').token);
+const bot = new Telegraf<MyContext>(require('./config.ts').default.token);
 
 // ---------------- PERSISTENCE ------------------
 
@@ -60,6 +63,7 @@ bot.use(session({
 
 bot.start(async (ctx) => {
     // set a default value
+
     ctx.session ??= {walletAddress: "", waitingForWalletAddress: false, language: 'EN'};
 
     ctx.session.waitingForWalletAddress = true;
@@ -79,119 +83,7 @@ const languages = [
     },
 ];
 
-const translations: any = {
-    'TOKENS': {
-        'EN': 'Select a token to trade',
-        'RU': 'Выберите токен для торговли'
-    },
-    'TOKEN': {
-        'EN': 'What would you like to do with XXX ⠀⠀⠀⠀⠀⠀⠀',
-        'RU': 'Что бы вы хотели сделать с XXX ⠀⠀⠀⠀⠀⠀⠀',
-    },
-    'TOKEN_TBD': {
-        'EN': 'XXX trading is coming soon!',
-        'RU': 'XXX, скоро начнутся торги!',
-    },
-    'HELP_TOKENS': {
-        'EN': 'To start trading on Tonswap you need to first select which token you want to trade. Tokens are normally traded against the native TON coin. After that, you will be able to do various actions such as buy, sell, add liquidity and claim rewards.',
-        'RU': 'Чтобы начать торговать на Tonswap, сначала нужно выбрать токен. Токенами обычно торгуют против базовой монеты TON. После этого, вы сможете выполнять различные действия, такие как покупка, продажа, добавление ликвидности и получение вознаграждения.',
 
-    },
-    'HELP_TOKEN': {
-        'EN': 'To swap XXX to TON press the Sell button. To swap TON to XXX press the Buy button. To add liquidity and earn rewards or to remove liquidity you already provided, tap the Manage Liquidity button',
-        'RU': 'Чтобы обменять XXX на TON, нажмите кнопку «Продать». Чтобы обменять TON на XXX, нажмите кнопку «Купить». Чтобы добавить ликвидность и заработать вознаграждение или удалить уже предоставленную ликвидность, нажмите кнопку «Управление ликвидностью»',
-    },
-    'HELP_LIQUIDITY': {
-        'EN': 'Adding liquidity is the action of supplying both XXX and TON to allow others to swap between them. For every swap you will earn 0.3% fees + additional 88% rewards APR. There is always a risk of impermanent loss so it is advised to learn about it before supplying liquidity. Remove liquidity is the action of getting back the XXX and TON you already supplied before.',
-        'RU': 'Добавление ликвидности — это действие по предоставлению как XXX, так и TON, чтобы позволить другим обмениваться ими. За каждый своп вы будете получать комиссию в размере 0,3% + дополнительные вознаграждения в размере 88% годовых. Всегда существует риск непоправимой потери, поэтому рекомендуется узнать об этом до предоставления ликвидности. Удаление ликвидности — это действие по возврату XXX и TON, которые вы уже предоставили ранее',
-    },
-    'HAS_REWARDS': {
-        'EN': 'You earned YYY XXX, tap to claim it 🥳',
-        'RU': 'Вы заработали YYY XXX, нажмите, чтобы получить 🥳',
-    },
-    'NO_REWARDS': {
-        'EN': 'Liquidity providers can earn rewards of up to 88% APR (yearly interest). You must add liquidity by depositing both XXX and TON to earn rewards',
-        'RU': 'Провайдеры ликвидности могут получать вознаграждение в размере до 88% годовых (годовых процентов). Вы должны увеличить ликвидность, внося как XXX, так и TON, чтобы получать вознаграждение',
-    },
-    'MANAGE_LIQUIDITY': {
-        'EN': 'Add liquidity by depositing both XXX and TON to earn rewards of up to 88% APR (yearly interest) or remove liquidity to withdraw your XXX and TON back to your wallet XXX and TON to earn rewards',
-        'RU': 'Bы можете добавить ликвидность, внося депозиты как XXX, так и TON, чтобы получить вознаграждение до 88% годовых (годовые проценты), или удалите ликвидность, чтобы вывести XXX и TON обратно в свой кошелек XXX и TON, чтобы получить вознаграждение',
-    },
-    'REMOVE_LIQUIDITY': {
-        'EN': 'You need to add liquidity before you can remove it',
-        'RU': 'Вам нужно добавить ликвидность, прежде чем вы сможете ее удалить',
-    },
-    'LANGUAGES': {
-        'EN': 'You are currently using XXX',
-        'RU': 'Вы используете язык XXX',
-    },
-    'DISCONNECTED': {
-        'EN': 'Connect your TON wallet by pasting your TON wallet address',
-        'RU': 'Подключите свой кошелек TON, вставив адрес своего кошелька TON',
-    },
-    'HELP_DISCONNECTED': {
-        'EN': 'In order to interact with TonSwap you need to provide your TON wallet address by writing it in this chat. This is the same address you give your friends when they want to send you TON coins.  . It looks something like EQCD39VS5jcptHL8vMjEXrzGaRcCVYto7HUn4bpAOg8xqB2N (this is TON foundation\'s wallet address).  To find your address, open your TON wallet (the app you use to manage your TON coins). Your wallet address usually appears in the front page of your walletor when tapping on the Receive button. Copy it and paste it in chat.',
-        'RU': 'Для взаимодействия с TonSwap, вам необходимо указать адрес своего кошелька TON, написав его в этом чате. Это тот же адрес, который вы даете своим друзьям, когда они хотят отправить вам TON. Он выглядит примерно так: EQCD39VS5jcptHL8vMjEXrzGaRcCVYto7HUn4bpAOg8xqB2N (это общий адрес кошелька TON). Чтобы найти личный адрес, откройте свой кошелек TON (приложение, которое вы используете для управления токенами TON). Адрес вашего кошелька обычно отображается на главной странице вашего кошелька или при нажатии на кнопку «Получить». Скопируйте его и вставьте в чат',
-    },
-    'WALLET_CONNECTED': {
-        'EN': 'Wallet connected successfully 🎉',
-        'RU': 'Кошелек успешно подключен 🎉'
-    },
-    'WALLET_DISCONNECTED': {
-        'EN': 'Wallet disconnected',
-        'RU': 'Кошелек отключен'
-    },
-    'INVALID_ADDRESS': {
-        'EN': 'Invalid wallet address, please try again',
-        'RU': 'Неверный адрес кошелька. Повторите попытку.'
-    },
-    buttons: {
-        'BUY': {
-            'EN': 'Buy XXX 💰',
-            'RU': 'Купить ХХХ 💰'
-        },
-        'SELL': {
-            'EN': 'Sell XXX 💸',
-            'RU': 'Продать ХХХ 💸'
-        },
-        'MANAGE_LIQUIDITY': {
-            'EN': 'Manage Liquidity 💧',
-            'RU': 'Управление ликвидностью 💧'
-        },
-        'CLAIM_REWARDS': {
-            'EN': 'Claim Rewards 💎',
-            'RU': 'Получить награду 💎'
-        },
-        'BACK': {
-            'EN': 'Back 🔙',
-            'RU': 'Назад 🔙'
-        },
-        'HELP': {
-            'EN': 'Help 🆘',
-            'RU': 'Помощь 🆘'
-        },
-        'GOT_IT': {
-            'EN': 'Got it 👍',
-            'RU': 'Ясно👍'
-        },
-        'ADD_LIQUIDITY': {
-            'EN': 'Add liquidity ➕',
-            'RU': 'Добавить ликвидность ➕'
-        },
-        'REMOVE_LIQUIDITY': {
-            'EN': 'Remove liquidity ➖',
-            'RU': 'Удалить ликвидность ➖'
-        },
-        'DISCONNECT': {
-            'EN': 'Disconnect 🔌',
-            'RU': 'Отключить 🔌'
-        },
-        'LANGUAGES': {
-            'EN': 'Languages XXX',
-            'RU': 'Языки XXX'
-        }
-    }
-}
 
 
 // ---------------- BUTTONS ------------------
@@ -235,15 +127,15 @@ const steps = {
         },
         buttons: (ctx: any) => {
             const token = ctx.update.callback_query.data.toUpperCase();
+                            
             return Markup.inlineKeyboard(
                 [
                     [
-                        Markup.button.url(prepareTranslation(translations.buttons.BUY[ctx.session.language], token), `${TONSWAP_URL}buy/${token.toLowerCase()}?telegram=true&address=${ctx.session.walletAddress}`),
-                        Markup.button.url(prepareTranslation(translations.buttons.SELL[ctx.session.language], token), `${TONSWAP_URL}sell/${token.toLowerCase()}?telegram=true&address=${ctx.session.walletAddress}`),
+                        Markup.button.webApp(prepareTranslation(translations.buttons.BUY[ctx.session.language], token), `${TONSWAP_URL}buy/${token.toLowerCase()}?telegram_webapp=true`),
+                        Markup.button.webApp(prepareTranslation(translations.buttons.SELL[ctx.session.language], token), `${TONSWAP_URL}sell/${token.toLowerCase()}?telegram_webapp=true`),
                     ],
                     [
                         Markup.button.callback(translations.buttons.MANAGE_LIQUIDITY[ctx.session.language], 'manage_liquidity'),
-                        Markup.button.callback(translations.buttons.CLAIM_REWARDS[ctx.session.language], 'claim_rewards'),
                     ],
                     [
                         Markup.button.callback(translations.buttons.BACK[ctx.session.language], 'back_token'),
@@ -291,37 +183,6 @@ const steps = {
             ]);
         }
     },
-    hasRewards: {
-        text: (ctx: any, rewards: number) => {
-            const token = ctx.session.token.toUpperCase();
-            const text = prepareTranslation(translations.HAS_REWARDS[ctx.session.language], token);
-            return text.replace('YYY', rewards.toString());
-        },
-        buttons: (ctx: any) => {
-            const token = ctx.session.token.toUpperCase();
-            return Markup.inlineKeyboard([
-                [
-                    Markup.button.url(translations.buttons.CLAIM_REWARDS[ctx.session.language], `${TONSWAP_URL}claim-rewards/${token.toLowerCase()}?telegram=true&address=${ctx.session.walletAddress}`),
-                    Markup.button.callback(translations.buttons.BACK[ctx.session.language], ctx.session.token),
-                ]
-            ]);
-        }
-    },
-    noRewards: {
-        text: (ctx: any) => {
-            const token = ctx.session.token.toUpperCase();
-            return prepareTranslation(translations.NO_REWARDS[ctx.session.language], token);
-        },
-        buttons: (ctx: any) => {
-            const token = ctx.session.token.toUpperCase();
-            return Markup.inlineKeyboard([
-                [
-                    Markup.button.url(translations.buttons.ADD_LIQUIDITY[ctx.session.language], `${TONSWAP_URL}add-liquidity/${token.toLowerCase()}?telegram=true&address=${ctx.session.walletAddress}`),
-                    Markup.button.callback(translations.buttons.BACK[ctx.session.language], ctx.session.token),
-                ]
-            ]);
-        }
-    },
     manageLiquidity: {
         text: (ctx: any) => {
             const token = ctx.session.token.toUpperCase();
@@ -332,13 +193,10 @@ const steps = {
             return Markup.inlineKeyboard(
                 [
                     [
-                        Markup.button.url(translations.buttons.ADD_LIQUIDITY[ctx.session.language], `${TONSWAP_URL}add-liquidity/${token.toLowerCase()}?telegram=true&address=${ctx.session.walletAddress}`),
+                        Markup.button.webApp(translations.buttons.ADD_LIQUIDITY[ctx.session.language], `${TONSWAP_URL}add-liquidity/${token.toLowerCase()}?telegram_webapp=true`),
                         hasLpBalance
-                            ? Markup.button.url(translations.buttons.REMOVE_LIQUIDITY[ctx.session.language], `${TONSWAP_URL}remove-liquidity/${token.toLowerCase()}?telegram=true&address=${ctx.session.walletAddress}`)
+                            ? Markup.button.webApp(translations.buttons.REMOVE_LIQUIDITY[ctx.session.language], `${TONSWAP_URL}remove-liquidity/${token.toLowerCase()}?telegram_webapp=true`)
                             : Markup.button.callback(translations.buttons.REMOVE_LIQUIDITY[ctx.session.language], 'remove_liquidity')
-                    ],
-                    [
-                        Markup.button.callback(translations.buttons.CLAIM_REWARDS[ctx.session.language], 'claim_rewards'),
                     ],
                     [
                         Markup.button.callback(translations.buttons.BACK[ctx.session.language], ctx.session.token),
@@ -380,9 +238,13 @@ const steps = {
         buttons: (ctx: any) => {
             return Markup.inlineKeyboard(
                 [
+                [
+                    Markup.button.callback(prepareTranslation(translations.buttons.SELECT_ACTION[ctx.session.language]), 'tokens')
+                ],
+                [
                     Markup.button.callback(prepareTranslation(translations.buttons.LANGUAGES[ctx.session.language], languages.find(l => l.id === ctx.session.language)?.label), 'languages'),
                     Markup.button.callback(translations.buttons.HELP[ctx.session.language], 'help_disconnected')
-                ]
+                ]]
             );
         }
     },
@@ -421,6 +283,25 @@ bot.action('languages', async (ctx: any) => {
     ctx.editMessageText(steps.languages.text(ctx), steps.languages.buttons(ctx));
 });
 
+
+bot.on('web_app_data', async (ctx: any) => {
+    console.log(ctx.update.message.web_app_data);
+    
+    // try {
+    //     ctx.reply(
+    //         '-',
+    //         Markup.inlineKeyboard([
+    //           Markup.button.webApp('Continue to your wallet 👛', ctx.update.message.web_app_data.data),
+    //         ])
+    //       )
+    // } catch (error) {
+    //         console.log(error);
+            
+    // }
+});
+
+
+
 bot.action('remove_liquidity', async (ctx: any) => {
     ctx.editMessageText(steps.remove_liquidity.text(ctx), steps.remove_liquidity.buttons(ctx));
 });
@@ -434,22 +315,11 @@ bot.action('manage_liquidity', async (ctx: any) => {
     ctx.editMessageText(steps.manageLiquidity.text(ctx), steps.manageLiquidity.buttons(ctx, lpBalance > 0));
 });
 
-bot.action('claim_rewards', async (ctx: any) => {
-    const token = ctx.session.token.toUpperCase();
-    const address = ctx.session.walletAddress;
-    const amm = tokens.tokens.find((t: Token) => t.name === token.toLowerCase()).amm;
-    const rewards = await getRewards(address, amm);
-
-    if (rewards > 0) {
-        ctx.editMessageText(steps.hasRewards.text(ctx, rewards), steps.hasRewards.buttons(ctx));
-    } else {
-        ctx.editMessageText(steps.noRewards.text(ctx), steps.noRewards.buttons(ctx));
-    }
-});
-
 bot.action('help_tokens', async (ctx: any) => {
     ctx.editMessageText(steps.helpTokens.text(ctx), steps.helpTokens.buttons(ctx));
 });
+
+
 
 bot.action('help_token', async (ctx: any) => {
     ctx.editMessageText(steps.helpToken.text(ctx), steps.helpToken.buttons(ctx));
@@ -524,31 +394,35 @@ const getTokenBalance = async (addressOfUser: string, amm: string) => {
     return parseNumber(new BN(eval(res.stack[0][1])));
 };
 
-const getRewards = async (addressOfUser: string, amm: string) => {
-    const owner = Address.parse(addressOfUser);
-    let wc = owner.workChain;
-    let address = new BN(owner.hash);
-    const res = await tonweb.call(amm, "get_rewards_of", [
-        ["num", wc.toString(10)],
-        ["num", address.toString(10)],
-    ]);
-
-    return parseNumber(new BN(eval(res.stack[0][1])), undefined, 10);
-};
 
 // ---------------- ON MESSAGE ------------------
 
+
+
+
 bot.on('message', async (ctx) => {
+        console.log((ctx.message as any).text);
+        
     // set a default value
     ctx.session ??= {walletAddress: "", waitingForWalletAddress: false, language: 'EN'};
 
     if (ctx.session.waitingForWalletAddress) {
         if (TonWeb.utils.Address.isValid((ctx.message as any).text)) {
             ctx.session.walletAddress = (ctx.message as any).text;
+            
+            try {
+                const balance = await tonweb.getBalance((ctx.message as any).text);
+                
+                ctx.session.walletBalance = balance
+            } catch (error) {
+                    console.log(error);
+                    
+            }
         }
     }
 
     if (ctx.session.walletAddress) {
+
         ctx.session.waitingForWalletAddress = false;
         await ctx.reply(translations.WALLET_CONNECTED[ctx.session.language!!]);
         await ctx.reply(steps.tokens.text(ctx), steps.tokens.buttons(ctx));
@@ -561,6 +435,16 @@ bot.on('message', async (ctx) => {
         }
     }
 });
+
+bot.on('poll', (ctx: any) => {
+    console.log(ctx);
+})
+
+
+bot.action('buy_click', (ctx: any) => {
+   ctx.re
+})
+
 
 // ---------------- BOT ------------------
 
---
+++ src/translations.ts
@@ -0,0 +1,108 @@
+const translations: any = {
+ 
+  "TOKENS": {
+    "EN": "Select a token to trade",
+    "RU": "Выберите токен для торговли"
+  },
+  "TOKEN": {
+    "EN": "What would you like to do with XXX ⠀⠀⠀⠀⠀⠀⠀",
+    "RU": "Что бы вы хотели сделать с XXX ⠀⠀⠀⠀⠀⠀⠀"
+  },
+  "TOKEN_TBD": {
+    "EN": "XXX trading is coming soon!",
+    "RU": "XXX, скоро начнутся торги!"
+  },
+  "HELP_TOKENS": {
+    "EN": "To start trading on Tonswap you need to first select which token you want to trade. Tokens are normally traded against the native TON coin. After that, you will be able to do various actions such as buy, sell, add liquidity and claim rewards.",
+    "RU": "Чтобы начать торговать на Tonswap, сначала нужно выбрать токен. Токенами обычно торгуют против базовой монеты TON. После этого, вы сможете выполнять различные действия, такие как покупка, продажа, добавление ликвидности и получение вознаграждения."
+  },
+  "HELP_TOKEN": {
+    "EN": "To swap XXX to TON press the Sell button. To swap TON to XXX press the Buy button. To add liquidity and earn rewards or to remove liquidity you already provided, tap the Manage Liquidity button",
+    "RU": "Чтобы обменять XXX на TON, нажмите кнопку «Продать». Чтобы обменять TON на XXX, нажмите кнопку «Купить». Чтобы добавить ликвидность и заработать вознаграждение или удалить уже предоставленную ликвидность, нажмите кнопку «Управление ликвидностью»"
+  },
+  "HELP_LIQUIDITY": {
+    "EN": "Adding liquidity is the action of supplying both XXX and TON to allow others to swap between them. For every swap you will earn 0.3% fees + additional 88% rewards APR. There is always a risk of impermanent loss so it is advised to learn about it before supplying liquidity. Remove liquidity is the action of getting back the XXX and TON you already supplied before.",
+    "RU": "Добавление ликвидности — это действие по предоставлению как XXX, так и TON, чтобы позволить другим обмениваться ими. За каждый своп вы будете получать комиссию в размере 0,3% + дополнительные вознаграждения в размере 88% годовых. Всегда существует риск непоправимой потери, поэтому рекомендуется узнать об этом до предоставления ликвидности. Удаление ликвидности — это действие по возврату XXX и TON, которые вы уже предоставили ранее"
+  },
+  "MANAGE_LIQUIDITY": {
+    "EN": "Add liquidity by depositing both XXX and TON to earn rewards of up to 88% APR (yearly interest) or remove liquidity to withdraw your XXX and TON back to your wallet XXX and TON to earn rewards",
+    "RU": "Bы можете добавить ликвидность, внося депозиты как XXX, так и TON, чтобы получить вознаграждение до 88% годовых (годовые проценты), или удалите ликвидность, чтобы вывести XXX и TON обратно в свой кошелек XXX и TON, чтобы получить вознаграждение"
+  },
+  "REMOVE_LIQUIDITY": {
+    "EN": "You need to add liquidity before you can remove it",
+    "RU": "Вам нужно добавить ликвидность, прежде чем вы сможете ее удалить"
+  },
+  "LANGUAGES": {
+    "EN": "You are currently using XXX",
+    "RU": "Вы используете язык XXX"
+  },
+  "DISCONNECTED": {
+    "EN": "Connect your TON wallet by pasting your TON wallet address",
+    "RU": "Подключите свой кошелек TON, вставив адрес своего кошелька TON"
+  },
+  "HELP_DISCONNECTED": {
+    "EN": "In order to interact with TonSwap you need to provide your TON wallet address by writing it in this chat. This is the same address you give your friends when they want to send you TON coins.  . It looks something like EQCD39VS5jcptHL8vMjEXrzGaRcCVYto7HUn4bpAOg8xqB2N (this is TON foundation's wallet address).  To find your address, open your TON wallet (the app you use to manage your TON coins). Your wallet address usually appears in the front page of your walletor when tapping on the Receive button. Copy it and paste it in chat.",
+    "RU": "Для взаимодействия с TonSwap, вам необходимо указать адрес своего кошелька TON, написав его в этом чате. Это тот же адрес, который вы даете своим друзьям, когда они хотят отправить вам TON. Он выглядит примерно так: EQCD39VS5jcptHL8vMjEXrzGaRcCVYto7HUn4bpAOg8xqB2N (это общий адрес кошелька TON). Чтобы найти личный адрес, откройте свой кошелек TON (приложение, которое вы используете для управления токенами TON). Адрес вашего кошелька обычно отображается на главной странице вашего кошелька или при нажатии на кнопку «Получить». Скопируйте его и вставьте в чат"
+  },
+  "WALLET_CONNECTED": {
+    "EN": "Wallet connected successfully 🎉",
+    "RU": "Кошелек успешно подключен 🎉"
+  },
+  "WALLET_DISCONNECTED": {
+    "EN": "Wallet disconnected",
+    "RU": "Кошелек отключен"
+  },
+  "INVALID_ADDRESS": {
+    "EN": "Invalid wallet address, please try again",
+    "RU": "Неверный адрес кошелька. Повторите попытку."
+  },
+  "buttons": {
+    "SELECT_ACTION": {
+      "EN": "Select action",
+      "RU": "Выберите действие"
+    },
+    "BUY": {
+      "EN": "Buy XXX 💰",
+      "RU": "Купить ХХХ 💰"
+    },
+    "SELL": {
+      "EN": "Sell XXX 💸",
+      "RU": "Продать ХХХ 💸"
+    },
+    "MANAGE_LIQUIDITY": {
+      "EN": "Manage Liquidity 💧",
+      "RU": "Управление ликвидностью 💧"
+    },
+    "BACK": {
+      "EN": "Back 🔙",
+      "RU": "Назад 🔙"
+    },
+    "HELP": {
+      "EN": "Help 🆘",
+      "RU": "Помощь 🆘"
+    },
+    "GOT_IT": {
+      "EN": "Got it 👍",
+      "RU": "Ясно👍"
+    },
+    "ADD_LIQUIDITY": {
+      "EN": "Add liquidity ➕",
+      "RU": "Добавить ликвидность ➕"
+    },
+    "REMOVE_LIQUIDITY": {
+      "EN": "Remove liquidity ➖",
+      "RU": "Удалить ликвидность ➖"
+    },
+    "DISCONNECT": {
+      "EN": "Disconnect 🔌",
+      "RU": "Отключить 🔌"
+    },
+    "LANGUAGES": {
+      "EN": "Languages XXX",
+      "RU": "Языки XXX"
+    }
+  }
+}
+
+
+export default translations
